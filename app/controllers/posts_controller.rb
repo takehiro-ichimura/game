5f3db408b944d1620683921c05c10f4279b1f08b
Ryo <test@test.com>
2020-09-26 04:33:56 +0100

    ryo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order(updated_at: :asc) 
   end
 
   # GET /posts/1
